408a74e3c95761ace7ad4db25e529a2201e4847f
paulostation <paulostation0@gmail.com>
2018-03-31 03:44:06 -0300

    Disabled classification request from website
    Classification request coming from the front-end was created because the server sending tweets 24 hours a day would take all available space on cloudant. Since I'm cleaning the database every 30 minutes, this is not a problem anymore.
    
    Also, I tidied up stuff a little bit.
    
    Signed-off-by: paulostation <paulostation0@gmail.com>

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,141 +1,5 @@
 const socket = io();
 
-var pause = true;
-
-$(document).ready(() => {
-    $("#pause").click(() => {
-        pause = !pause;
-    })
-
-    $("#clear").click(() => {
-        $("#tweets").html("");
-    })
-
-    var ctx = document.getElementById("myChart").getContext('2d');
-
-    positiveTweets = [];
-    negativeTweets = [];
-
-    getTweets("positive")
-        .then(positive => {
-            positiveTweets=positive
-
-            return getTweets("negative");
-        })
-        .then( negative => {
-            negativeTweets = negative;
-
-            var myChart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    datasets: [{
-                        label: 'tweets positivos',
-                        data: positiveTweets,
-                        backgroundColor: 'rgba(0, 255, 0, 0.2)',
-                        borderWidth: 2
-    
-                    }
-                        ,
-                    {
-                        label: 'tweets negativos',
-                        data: negativeTweets,
-                        backgroundColor: 'rgba(255, 0, 0, 0.2)',
-                        borderWidth: 2
-                    }
-                    ]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            stacked: true,
-                            ticks: {
-                                source: "labels"
-                            }
-                        }],
-    
-                        xAxes: [{
-                            type: 'time',
-                            stacked: true,
-                            // barThickness: 6,
-                            // maxPercentage: 1,
-    
-                            distribution: "linear",
-                            time: {
-                                // string/callback - By default, date objects are expected. You may use a pattern string from http://momentjs.com/docs/#/parsing/string-format/ to parse a time string format, or use a callback function that is passed the label, and must return a moment() instance.
-                                parser: false,
-                                // string - By default, unit will automatically be detected.  Override with 'week', 'month', 'year', etc. (see supported time measurements)
-                                unit: 'minute',
-    
-                                // Number - The number of steps of the above unit between ticks
-                                // unitStepSize: 1,
-    
-                                // string - By default, no rounding is applied.  To round, set to a supported time unit eg. 'week', 'month', 'year', etc.
-                                round: true,
-    
-                                // Moment js for each of the units. Replaces `displayFormat`
-                                // To override, use a pattern string from http://momentjs.com/docs/#/displaying/format/
-                                displayFormats: {
-                                    // max: moment().startOf('year'),
-                                    // min: moment().endOf('year'),
-                                    'millisecond': 'SSS [ms]',
-                                    'second': 'h:mm:ss a', // 11:20:01 AM
-                                    'minute': 'h:mm:ss a', // 11:20:01 AM
-                                    'hour': 'MMM D, hA', // Sept 4, 5PM
-                                    'day': 'MMM Do', // Sep 4 2015
-                                    'week': 'll', // Week 46, or maybe "[W]WW - YYYY" ?
-                                    'month': 'MMM YYYY', // Sept 2015
-                                    'quarter': '[Q]Q - YYYY', // Q3
-                                    'year': 'YYYY', // 2015
-                                },
-                            }
-    
-    
-                        }],
-                    },
-    
-    
-                }
-            });
-
-        })
-        .catch(console.error)
-
-        
-
-
-
-
-});
-
-socket.on('tweet', tweet => {
-
-    if (pause !== false) {
-        getSentiment(tweet)
-            .then(result => {
-
-                result = JSON.parse(result);
-
-                if (result.sentiment.document.label === 'positive') {
-                    $("#tweets").prepend('<div style="background-color:lightgreen">' + tweet.text + "</div>");
-                }
-                else if (result.sentiment.document.label === 'negative') {
-                    $("#tweets").prepend('<div style="background-color:#ff5050">' + tweet.text + "</div>");
-                }
-                else {
-                    $("#tweets").prepend('<div style="background-color:lightgray">' + tweet.text + "</div>");
-                }
-
-                $("#tweets").prepend('<br>');
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-
-
-
-});
-
 function getSentiment(tweet) {
 
     return new Promise((resolve, reject) => {
@@ -147,12 +11,8 @@ function getSentiment(tweet) {
                 text: tweet.text
             },
             json: true,
-            error: error => {
-                reject(error);
-            },
-            success: data => {
-                resolve(data);
-            }
+            error: reject,
+            success: resolve
         });
 
     });
@@ -166,13 +26,102 @@ function getTweets(sentiment) {
             url: "/tweets/" + sentiment,
             method: "GET",
             json: true,
-            error: error => {
-                reject(error);
-            },
-            success: data => {
-                resolve(data);
-            }
+            error: reject,
+            success: resolve
         });
 
     });
 }
+
+function renderChart(positiveTweets, negativeTweets) {
+
+    var graphicContext = document.getElementById("myChart").getContext('2d');
+
+    var myChart = new Chart(graphicContext, {
+        type: 'bar',
+        data: {
+            datasets: [{
+                label: 'tweets positivos',
+                data: positiveTweets,
+                backgroundColor: 'rgba(0, 255, 0, 0.2)',
+                borderWidth: 2
+
+            },
+            {
+                label: 'tweets negativos',
+                data: negativeTweets,
+                backgroundColor: 'rgba(255, 0, 0, 0.2)',
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    stacked: true,
+                    ticks: {
+                        source: "labels"
+                    }
+                }],
+                xAxes: [{
+                    type: 'time',
+                    stacked: true,
+                    distribution: "linear",
+                    time: {
+                        // string/callback - By default, date objects are expected. You may use a pattern string from http://momentjs.com/docs/#/parsing/string-format/ to parse a time string format, or use a callback function that is passed the label, and must return a moment() instance.
+                        parser: false,
+                        // string - By default, unit will automatically be detected.  Override with 'week', 'month', 'year', etc. (see supported time measurements)
+                        unit: 'hour',
+                        // Moment js for each of the units. Replaces `displayFormat`
+                        // To override, use a pattern string from http://momentjs.com/docs/#/displaying/format/
+                        displayFormats: {
+                            'millisecond': 'SSS [ms]',
+                            'second': 'h:mm:ss a', // 11:20:01 AM
+                            'minute': 'h:mm:ss a', // 11:20:01 AM
+                            'hour': 'MMM D, hA', // Sept 4, 5PM
+                            'day': 'MMM Do', // Sep 4 2015
+                            'week': 'll', // Week 46, or maybe "[W]WW - YYYY" ?
+                            'month': 'MMM YYYY', // Sept 2015
+                            'quarter': '[Q]Q - YYYY', // Q3
+                            'year': 'YYYY', // 2015
+                        },
+                    }
+                }],
+            },
+        }
+    });
+}
+
+$(document).ready(() => {
+
+    positiveTweets = [];
+    negativeTweets = [];
+
+    getTweets("positive")
+        .then(positive => {
+            positiveTweets = positive
+
+            return getTweets("negative");
+        })
+        .then(negative => {
+            negativeTweets = negative;
+
+            renderChart(positiveTweets, negativeTweets);
+
+        })
+        .catch(console.error)
+});
+
+socket.on('tweet', tweet => {
+
+    if (tweet.analysis === 'positive') {
+        $("#tweets").prepend('<div style="background-color:lightgreen">' + tweet.text + "</div>");
+    }
+    else if (tweet.analysis === 'negative') {
+        $("#tweets").prepend('<div style="background-color:#ff5050">' + tweet.text + "</div>");
+    }
+    else {
+        $("#tweets").prepend('<div style="background-color:lightgray">' + tweet.text + "</div>");
+    }
+
+    $("#tweets").prepend('<br>');
+});
