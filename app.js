4b093355f31e7031714b67ddb09d418a85c281c8
paulostation <paulostation0@gmail.com>
2021-10-14 20:28:54 -0300

    Barebones version without need of DB connection
    Signed-off-by: paulostation <paulostation0@gmail.com>

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+// var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 const schedule = require("node-schedule");
-const winston_logger = require("./util/logger.js");
+// const winston_logger = require("./util/logger.js");
 
-const index = require('./routes/index'),
-  users = require('./routes/users'),
-  status = require('./routes/status'),
-  tweets = require('./routes/tweets');
+const index = require('./routes/index');
+//   users = require('./routes/users'),
+//   status = require('./routes/status'),
+//   tweets = require('./routes/tweets');
 
 var app = express();
 
@@ -23,16 +23,16 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/status', status);
-app.use('/tweets', tweets);
+// app.use('/users', users);
+// app.use('/status', status);
+// app.use('/tweets', tweets);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -42,47 +42,47 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+// app.use(function (err, req, res, next) {
 
-  winston_logger.error(err.message);
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   winston_logger.error(err.message);
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
-var j = schedule.scheduleJob('1 * * * *', function () {
+// var j = schedule.scheduleJob('1 * * * *', function () {
 
-  winston_logger.debug("Calling child process...");
+//   winston_logger.debug("Calling child process...");
 
-  const { spawn } = require('child_process');
+//   const { spawn } = require('child_process');
 
-  const deleteOldTweets = spawn('node', [path.join(__dirname, "./clearDB.js")], {});
+//   const deleteOldTweets = spawn('node', [path.join(__dirname, "./clearDB.js")], {});
 
-  let stderr = "";
+//   let stderr = "";
 
-  deleteOldTweets.stdout.on('data', data => {
+//   deleteOldTweets.stdout.on('data', data => {
 
-    winston_logger.debug(data.toString());
+//     winston_logger.debug(data.toString());
 
-  });
+//   });
 
-  deleteOldTweets.stderr.on('data', data => {
+//   deleteOldTweets.stderr.on('data', data => {
 
-    stderr += data;
+//     stderr += data;
 
-  });
+//   });
 
-  deleteOldTweets.on('close', code => {
+//   deleteOldTweets.on('close', code => {
 
-    if (code !== 0)
-      winston_logger.error(stderr);
+//     if (code !== 0)
+//       winston_logger.error(stderr);
 
-    winston_logger.debug(`child process exited with code ${code}`);
-  });
-});
+//     winston_logger.debug(`child process exited with code ${code}`);
+//   });
+// });
 
 module.exports = app;
